20d8b53b567fd9c3284372b5030a390d8dfd12d2
Mary <kass.o@yandex.ru>
2024-05-19 14:04:42 +0300

    admin account has been opened

--- habits/admin.py
+++ habits/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from habits.models import Habit
+
+
+@admin.register(Habit)
+class WellAdmin(admin.ModelAdmin):
+    list_display = ('user', 'place', 'time', 'action', 'sign_good_habit', 'related_habits', 'periodicity', 'reward',
+                    'time_to_complete', 'is_public')
+    search_fields = ('sign_good_habit', 'is_public',)
