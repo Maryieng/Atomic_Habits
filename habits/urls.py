fa65aba091d75f9584d97c9632a3dee7b87b946a
Mary <kass.o@yandex.ru>
2024-05-23 16:53:51 +0300

    editing validators

--- habits/urls.py
+++ habits/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
 
-from habits.views import (HabitCreateView, HabitListView, HabitRetrieveView, HabitUpdateView, HabitDestroyView)
+from habits.views import (HabitCreateView, HabitListView, HabitRetrieveView, HabitUpdateView, HabitDestroyView,
+                          GetPublicHabitListView)
 
 app_name = 'habits'
 
@@ -10,6 +11,7 @@ router = DefaultRouter()
 urlpatterns = [
     path('create/', HabitCreateView.as_view(), name='habit_create'),
     path('', HabitListView.as_view(), name='habit_list'),
+    path('public_list', GetPublicHabitListView.as_view(), name='public_habit_list'),
     path('view/<int:pk>/', HabitRetrieveView.as_view(), name='habit_view'),
     path('update/<int:pk>/', HabitUpdateView.as_view(), name='habit_update'),
     path('delete/<int:pk>/', HabitDestroyView.as_view(), name='habit_delete')] + router.urls
